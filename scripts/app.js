5073e633835dca89c4412a84c86952793ba690c7
GalisGalisteo <galis.travelling@gmail.com>
2023-10-26 09:13:43 +0200

    starting ex

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,19 @@
 /** TODO: Viene del inglés: "to do", es decir, una tarea que falta por hacer */
+
+const userInput = document.querySelector('#user-input');
+const submitBtn = document.querySelector('#submit-btn');
+const todosDiv = document.querySelector('.list-item__container');
+
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    const userInputValue = userInput.value;
+    userInput.value = '';
+    const listItem = document.createElement('div');
+    listItem.classList.add('list-item');
+    listItem.textContent = userInputValue;
+    todosDiv.appendChild(listItem);
+    listItem.addEventListener('dblclick', () => {
+        listItem.classList.toggle('list-item__done');
+    })
+    
+})
