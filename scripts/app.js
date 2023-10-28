9b8e10373d32f07082e2ae22390a80e225be047e
GalisGalisteo <galis.travelling@gmail.com>
2023-10-28 19:35:50 +0200

    adding todoTask array and task object

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,52 @@
 /** TODO: Viene del inglés: "to do", es decir, una tarea que falta por hacer */
 
-const userInput = document.querySelector('#user-input');
-const submitBtn = document.querySelector('#submit-btn');
-const todosDiv = document.querySelector('.list-item__container');
 
+const todoTasks = [];
 
-submitBtn.addEventListener('click', (event) => { // hay que cambiarlo por 'submit'
+const todoForm = document.querySelector('#todo-form');
 
+todoForm.addEventListener('submit', (event) => {
     event.preventDefault()
+
+    const userInput = document.querySelector('#user-input');
+    const todosDiv = document.querySelector('.list-item__container');
+
     const userInputValue = userInput.value;
+    const task = {
+        name: userInputValue,
+        isCompleted: false
+    }
+    todoTasks.push(task);
     userInput.value = '';
+
     const listItem = document.createElement('div');
     listItem.classList.add('list-item');
     listItem.textContent = userInputValue;
     todosDiv.appendChild(listItem);
+
     listItem.addEventListener('dblclick', () => {
+        const taskName = listItem.textContent;
+        console.log("🚀 ~ file: app.js:29 ~ listItem.addEventListener ~ task:", task)
+        todoTasks.forEach(todoTask => {
+            if (todoTask.name === taskName) {
+                todoTask.isCompleted = true;
+                console.log("🚀 ~ file: app.js:33 ~ listItem.addEventListener ~ todoTask:", todoTask)
+            }
+        });
         listItem.classList.toggle('list-item__done');
     })
 
+    const hideCompleted = document.querySelector('#hide-completed');
+    hideCompleted.addEventListener('change', () => {
+        const listItemDone = document.querySelectorAll('.list-item__done');
+        if (hideCompleted.checked) {
+            listItemDone.forEach(item => {
+                item.style.display = 'none';
+            })
+        } else {
+            listItemDone.forEach(item => {
+                item.style.display = 'initial';
+            })
+        }
+    })
 })
